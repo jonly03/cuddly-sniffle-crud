3e912dd92b1348ddcc0906f67d6f507a2de47483
Nelly Sugu <jonly03@gmail.com>
2020-12-08 16:11:08 -0500

    added generic port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express();
 
 server.use(bodyParser.json());
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 
 const employees = []; //[{fName: "Matt", lName: "Sugu", email: "matt.s@amazon.com", role: "sde", eId: "123432"}]
 
@@ -16,29 +16,31 @@ server.post("/employees", (req, res) => {
 //route to return list of all employees
 server.get("/employees", (req, res) => {
   res.send(employees);
-})
+});
 
 //route to return employees by role
 server.get("/employees/:role", (req, res) => {
   const role = req.params.role;
-  const results = employees.filter(employee => employee.role.toUpperCase() === role.toUpperCase());
+  const results = employees.filter(
+    (employee) => employee.role.toUpperCase() === role.toUpperCase()
+  );
 
   res.send(results);
-})
+});
 
 //route to return employees by id
 server.get("/employees/:id", (req, res) => {
   const eId = req.params.id;
-  const results = employees.filter(emp => emp.eId === eId);
+  const results = employees.filter((emp) => emp.eId === eId);
 
   res.send(results);
-})
+});
 
 //route to change employees information by id
 server.put("/employees/:id", (req, res) => {
   const id = req.params.id;
   const employee = req.body;
-  let result = employees.filter(emp => emp.eId === id)
+  let result = employees.filter((emp) => emp.eId === id);
   if (employee.fName !== undefined) {
     result[0].fName = employee.fName;
   }
@@ -52,7 +54,7 @@ server.put("/employees/:id", (req, res) => {
     result[0].role = employee.role;
   }
   res.send(result[0]);
-})
+});
 
 //route to delete employees by id
 server.delete("/employees/:id", (req, res) => {
@@ -64,10 +66,10 @@ server.delete("/employees/:id", (req, res) => {
       empIdx = idx;
       return;
     }
-  })
+  });
   if (empIdx === -1) {
     return res.status(404).send("Employee not found");
   }
   employees.splice(empIdx, 1);
-  res.send({success: "Success"});
-})
+  res.send({ success: "Success" });
+});
