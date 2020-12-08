93010605e2917aaaf023d414a155fcd2d35320fc
Nelly Sugu <jonly03@gmail.com>
2020-12-08 17:37:12 -0500

    enable cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const server = express();
 
 server.use(bodyParser.json());
+server.use(cors());
 
 server.listen(process.env.PORT || 3000);
 
